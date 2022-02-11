49a59c68153f2091b755f3422aad074e58f8debf
Ricardo Lamadrid <ricardo.lamadrid@bldr.com>
2022-02-11 12:01:05 -0600

    moved shared into modulefed plugin

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -16,8 +16,8 @@ const prodConfig = {
       exposes: {
         './MarketingApp': './src/bootstrap',
       },
+      shared: packageJson.dependencies,
     }),
-    shared: packageJson.dependencies,
   ],
 };
 
