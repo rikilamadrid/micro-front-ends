4b005f7d08b6056c64534e6bcd7e9513490910ea
Ricardo Lamadrid <ricardo.lamadrid@bldr.com>
2022-02-15 10:09:55 -0600

    typo 2

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import MarketingApp from './MarketingApp';
+import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
 export default function App() {
